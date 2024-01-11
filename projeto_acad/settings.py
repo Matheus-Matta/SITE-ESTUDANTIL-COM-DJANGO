bddbf952b6b1bf0ac5beae5f461c5ffe7bc76e6d
matheus <matheuseduardo3004@gmail.com>
2024-01-10 23:14:13 -0300

    Mensagem do commit

--- projeto_acad/settings.py
+++ projeto_acad/settings.py
@@ -39,17 +39,23 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'app_acad'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://matheusmatta.pythonanywhere.com",
 ]
 
 ROOT_URLCONF = 'projeto_acad.urls'
